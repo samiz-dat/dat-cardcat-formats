1b6bad32325e3b4595ff1d2021a5385f714c9eb6
sdockray <sean.patrick.dockray@gmail.com>
2017-08-22 17:50:24 +1000

    added Open Media Library; multiple authors for Calibre; Calibre sorting error

--- src/formats.js
+++ src/formats.js
@@ -11,23 +11,38 @@ import { joinName, parseName, parseNames, alphabetizedTitle } from './utils';
 // Files to ignore, even if they are in the right place
 const ignore = ['.DS_Store', '.dat', '.git', 'nohup.out'];
 // The possible parser choices available
-const choices = ['calibre', 'flat'];
+const choices = ['oml', 'calibre', 'flat'];
 
 // These might reformat the input data to create a path.
 // This newly created path will be reversible: parser <--> formatter
 const formatters = {
   // Every part of calibre path structure is downloadable
-  calibre: (authors, title, file) => path.join(
-    joinName(parseName(_.first(authors)), { includeRole: true }),
-    title,
-    file,
-  ),
+  calibre: (authors, title, file) => {
+    const formatName = n => joinName(parseName(n), { includeRole: true });
+    const authorPart = _.join(_.take(authors, 3).map(a => formatName(a)), ', ');
+    return path.join(
+      authorPart,
+      title,
+      file,
+    );
+  },
   // With flat files the filename is discarded and the title is used.
   flat: (authors, title, file) => {
     const formatName = n => joinName(parseName(n), { alphabetical: true, includeRole: true });
     const ext = path.extname(file);
     const authorPart = _.join(_.take(authors, 3).map(a => formatName(a)), '; ');
     return `${authorPart} - ${title}${ext}`;
+  },
+  // Open Media Library
+  oml: (authors, title, file) => {
+    const formatName = n => joinName(parseName(n), { alphabetical: true, includeRole: true });
+    const ext = path.extname(file);
+    const authorPart = _.join(_.take(authors, 3).map(a => formatName(a)), '; ');
+    return path.join(
+      authorPart.charAt(0).toUpperCase(),
+      authorPart,
+      `${title}${ext}`,
+    );
   },
 };
 
@@ -36,21 +51,24 @@ const parsers = {
   // Calibre parser is the default one
   calibre: (pathArr, filePath) => {
     if (pathArr.length === 3 && !ignore.includes(pathArr[2])) {
-      const parsed = parseNames([pathArr[0]]);
+      const authorNames = pathArr[0].split(',');
+      const parsed = parseNames(authorNames);
       const authors = parsed.map(n => ({
         author: joinName(n),
-        author_sort: n.original,
+        author_sort: joinName(n, { alphabetical: true }),
         role: n.role || undefined,
       }));
-      return {
-        authors,
-        author_sort: joinName(authors[0].author_sort, { alphabetical: true }),
-        title: pathArr[1],
-        title_sort: alphabetizedTitle(pathArr[1]),
-        file: pathArr[2],
-        path: filePath,
-        format: 'calibre',
-      };
+      if (authors.length) {
+        return {
+          authors,
+          author_sort: authors[0].author_sort,
+          title: pathArr[1],
+          title_sort: alphabetizedTitle(pathArr[1]),
+          file: pathArr[2],
+          path: filePath,
+          format: 'calibre',
+        };
+      }
     }
     return false;
   },
@@ -80,6 +98,43 @@ const parsers = {
             format: 'flat',
           };
         }
+      }
+    }
+    return false;
+  },
+  // The OML style is "Letter/Sorted Author/Title (Publisher Year).ext"
+  // @TODO: handle extra information in paths
+  oml: (pathArr, filePath) => {
+    if (pathArr.length === 3
+      && pathArr[0].length === 1
+      && pathArr[0].charAt(0).toLowerCase() === pathArr[1].charAt(0).toLowerCase()
+      && !ignore.includes(pathArr[2])) {
+      const authorNames = pathArr[1].split(';');
+      const parsed = parseNames(authorNames);
+      const file = pathArr[2];
+      const ext = path.extname(file);
+      let title = file.replace(ext, '');
+      // extras would be (Publisher Year)
+      const extrasMatch = title.match(/\(([^)]*)\)[^(]*$/);
+      if (extrasMatch && extrasMatch[1]) {
+        title = title.replace(/ *\([^)]*\) */g, '');
+        // const extras = extrasMatch[1];
+      }
+      const authors = parsed.map(n => ({
+        author: joinName(n),
+        author_sort: n.original,
+        role: n.role || undefined,
+      }));
+      if (authors.length && title) {
+        return {
+          authors,
+          author_sort: authors[0].author_sort,
+          title,
+          title_sort: alphabetizedTitle(title),
+          file,
+          path: filePath,
+          format: 'oml',
+        };
       }
     }
     return false;
--- test/parsers.js
+++ test/parsers.js
@@ -15,17 +15,139 @@ const invalidPaths = [
   'one/two/three/four.pdf',
 ];
 
-const validPaths = [
-  'Deleuze, Gilles; Guattari, Felix - A Thousand Plateaus.opf',
-  'Edward Said/After Colonialism: Imperial Histories and Postcolonial Displacements/metadata.opf',
-  'Said, Edward - After Colonialism: Imperial Histories and Postcolonial Displacements.opf',
-  'BAVO (eds.)/Cultural Activism Today. The Art of Over-Identification/metadata.opf',
-  'BAVO (eds.) - Cultural Activism Today. The Art of Over-Identification.opf',
-  'Susan Buck-Morss/Hegel y Haití. La dialéctica amo-esclavo, una interpretación revolucionaria (Spanish)/metadata.opf',
-  'Buck-Morss, Susan - Hegel y Haití. La dialéctica amo-esclavo, una interpretación revolucionaria (Spanish).opf',
-  '/Edward Said/After Colonialism: Imperial Histories and Postcolonial Displacements/metadata.opf',
-  '/Said, Edward - After Colonialism: Imperial Histories and Postcolonial Displacements.opf',
-  'Buden, Boris; Žilnik, Želimir; kuda.org, et al. - Uvod u prošlost (Serbian).opf',
+const testData = [
+  {
+    test: 'Deleuze, Gilles; Guattari, Felix - A Thousand Plateaus.opf',
+    author: 'Gilles Deleuze',
+    author_sort: 'Deleuze, Gilles',
+    role: undefined,
+    title: 'A Thousand Plateaus',
+    title_sort: 'Thousand Plateaus, A',
+    file: 'Deleuze, Gilles; Guattari, Felix - A Thousand Plateaus.opf',
+    format: 'flat',
+    authorCount: 2,
+  },
+  {
+    test: 'Edward Said/After Colonialism: Imperial Histories and Postcolonial Displacements/metadata.opf',
+    author: 'Edward Said',
+    author_sort: 'Said, Edward',
+    role: undefined,
+    title: 'After Colonialism: Imperial Histories and Postcolonial Displacements',
+    title_sort: 'After Colonialism: Imperial Histories and Postcolonial Displacements',
+    file: 'metadata.opf',
+    format: 'calibre',
+    authorCount: 1,
+  },
+  {
+    test: 'Said, Edward - After Colonialism: Imperial Histories and Postcolonial Displacements.opf',
+    author: 'Edward Said',
+    author_sort: 'Said, Edward',
+    role: undefined,
+    title: 'After Colonialism: Imperial Histories and Postcolonial Displacements',
+    title_sort: 'After Colonialism: Imperial Histories and Postcolonial Displacements',
+    file: 'Said, Edward - After Colonialism: Imperial Histories and Postcolonial Displacements.opf',
+    format: 'flat',
+    authorCount: 1,
+  },
+  {
+    test: 'BAVO (eds.)/Cultural Activism Today. The Art of Over-Identification/metadata.opf',
+    author: 'BAVO',
+    author_sort: 'BAVO',
+    role: 'editor',
+    title: 'Cultural Activism Today. The Art of Over-Identification',
+    title_sort: 'Cultural Activism Today. The Art of Over-Identification',
+    file: 'metadata.opf',
+    format: 'calibre',
+    authorCount: 1,
+  },
+  {
+    test: 'BAVO (eds.) - Cultural Activism Today. The Art of Over-Identification.opf',
+    author: 'BAVO',
+    author_sort: 'BAVO',
+    role: 'editor',
+    title: 'Cultural Activism Today. The Art of Over-Identification',
+    title_sort: 'Cultural Activism Today. The Art of Over-Identification',
+    file: 'BAVO (eds.) - Cultural Activism Today. The Art of Over-Identification.opf',
+    format: 'flat',
+    authorCount: 1,
+  },
+  {
+    test: 'Susan Buck-Morss/Hegel y Haití. La dialéctica amo-esclavo, una interpretación revolucionaria (Spanish)/metadata.opf',
+    author: 'Susan Buck-Morss',
+    author_sort: 'Buck-Morss, Susan',
+    role: undefined,
+    title: 'Hegel y Haití. La dialéctica amo-esclavo, una interpretación revolucionaria (Spanish)',
+    title_sort: 'Hegel y Haití. La dialéctica amo-esclavo, una interpretación revolucionaria (Spanish)',
+    file: 'metadata.opf',
+    format: 'calibre',
+    authorCount: 1,
+  },
+  {
+    test: 'Buck-Morss, Susan - Hegel y Haití. La dialéctica amo-esclavo, una interpretación revolucionaria (Spanish).opf',
+    author: 'Susan Buck-Morss',
+    author_sort: 'Buck-Morss, Susan',
+    role: undefined,
+    title: 'Hegel y Haití. La dialéctica amo-esclavo, una interpretación revolucionaria (Spanish)',
+    title_sort: 'Hegel y Haití. La dialéctica amo-esclavo, una interpretación revolucionaria (Spanish)',
+    file: 'Buck-Morss, Susan - Hegel y Haití. La dialéctica amo-esclavo, una interpretación revolucionaria (Spanish).opf',
+    format: 'flat',
+    authorCount: 1,
+  },
+  {
+    test: '/Edward Said/After Colonialism: Imperial Histories and Postcolonial Displacements/metadata.opf',
+    author: 'Edward Said',
+    author_sort: 'Said, Edward',
+    role: undefined,
+    title: 'After Colonialism: Imperial Histories and Postcolonial Displacements',
+    title_sort: 'After Colonialism: Imperial Histories and Postcolonial Displacements',
+    file: 'metadata.opf',
+    format: 'calibre',
+    authorCount: 1,
+  },
+  {
+    test: '/Said, Edward - After Colonialism: Imperial Histories and Postcolonial Displacements.opf',
+    author: 'Edward Said',
+    author_sort: 'Said, Edward',
+    role: undefined,
+    title: 'After Colonialism: Imperial Histories and Postcolonial Displacements',
+    title_sort: 'After Colonialism: Imperial Histories and Postcolonial Displacements',
+    file: 'Said, Edward - After Colonialism: Imperial Histories and Postcolonial Displacements.opf',
+    format: 'flat',
+    authorCount: 1,
+  },
+  {
+    test: 'Buden, Boris; Žilnik, Želimir; kuda.org, et al. - Uvod u prošlost (Serbian).opf',
+    author: 'Boris Buden',
+    author_sort: 'Buden, Boris',
+    role: undefined,
+    title: 'Uvod u prošlost (Serbian)',
+    title_sort: 'Uvod u prošlost (Serbian)',
+    file: 'Buden, Boris; Žilnik, Želimir; kuda.org, et al. - Uvod u prošlost (Serbian).opf',
+    format: 'flat',
+    authorCount: 3,
+  },
+  {
+    test: 'C/Chevalier (Editor), Tracy/Encyclopedia of the Essay.opf',
+    author: 'Tracy Chevalier',
+    author_sort: 'Chevalier, Tracy',
+    role: 'Editor',
+    title: 'Encyclopedia of the Essay',
+    title_sort: 'Encyclopedia of the Essay',
+    file: 'Encyclopedia of the Essay.opf',
+    format: 'oml',
+    authorCount: 1,
+  },
+  {
+    test: 'L/Laclau, Ernesto; Mouffe, Chantal/Hegemony and Socialist Strategy (1985).opf',
+    author: 'Ernesto Laclau',
+    author_sort: 'Laclau, Ernesto',
+    role: undefined,
+    title: 'Hegemony and Socialist Strategy',
+    title_sort: 'Hegemony and Socialist Strategy',
+    file: 'Hegemony and Socialist Strategy (1985).opf',
+    format: 'oml',
+    authorCount: 2,
+  },
 ];
 
 const formattedPaths = [
@@ -35,6 +157,7 @@ const formattedPaths = [
     file: 'metadata.opf',
     calibre: 'Edward Said/After Colonialism: Imperial Histories and Postcolonial Displacements/metadata.opf',
     flat: 'Said, Edward - After Colonialism: Imperial Histories and Postcolonial Displacements.opf',
+    oml: 'S/Said, Edward/After Colonialism: Imperial Histories and Postcolonial Displacements.opf',
   },
   {
     authors: ['BAVO (eds.)'],
@@ -42,6 +165,7 @@ const formattedPaths = [
     file: 'metadata.opf',
     calibre: 'BAVO (editors)/Cultural Activism Today. The Art of Over-Identification/metadata.opf',
     flat: 'BAVO (editors) - Cultural Activism Today. The Art of Over-Identification.opf',
+    oml: 'B/BAVO (editors)/Cultural Activism Today. The Art of Over-Identification.opf',
   },
   {
     authors: ['Susan Buck-Morss'],
@@ -49,141 +173,55 @@ const formattedPaths = [
     file: 'metadata.opf',
     calibre: 'Susan Buck-Morss/Hegel y Haití. La dialéctica amo-esclavo, una interpretación revolucionaria (Spanish)/metadata.opf',
     flat: 'Buck-Morss, Susan - Hegel y Haití. La dialéctica amo-esclavo, una interpretación revolucionaria (Spanish).opf',
+    oml: 'B/Buck-Morss, Susan/Hegel y Haití. La dialéctica amo-esclavo, una interpretación revolucionaria (Spanish).opf',
   },
   {
     authors: ['Boris Buden', 'Želimir Žilnik', 'kuda.org'],
     title: 'Uvod u prošlost (Serbian)',
     file: 'metadata.opf',
-    calibre: 'Boris Buden/Uvod u prošlost (Serbian)/metadata.opf',
+    calibre: 'Boris Buden, Želimir Žilnik, kuda.org/Uvod u prošlost (Serbian)/metadata.opf',
     flat: 'Buden, Boris; Žilnik, Želimir; kuda.org - Uvod u prošlost (Serbian).opf',
+    oml: 'B/Buden, Boris; Žilnik, Želimir; kuda.org/Uvod u prošlost (Serbian).opf',
   },
 ];
 
-const correctData = [
-  {
-    author: 'Gilles Deleuze',
-    role: undefined,
-    title: 'A Thousand Plateaus',
-    title_sort: 'Thousand Plateaus, A',
-    file: 'Deleuze, Gilles; Guattari, Felix - A Thousand Plateaus.opf',
-    format: 'flat',
-    authorCount: 2,
-  },
-  {
-    author: 'Edward Said',
-    role: undefined,
-    title: 'After Colonialism: Imperial Histories and Postcolonial Displacements',
-    title_sort: 'After Colonialism: Imperial Histories and Postcolonial Displacements',
-    file: 'metadata.opf',
-    format: 'calibre',
-    authorCount: 1,
-  },
-  {
-    author: 'Edward Said',
-    role: undefined,
-    title: 'After Colonialism: Imperial Histories and Postcolonial Displacements',
-    title_sort: 'After Colonialism: Imperial Histories and Postcolonial Displacements',
-    file: 'Said, Edward - After Colonialism: Imperial Histories and Postcolonial Displacements.opf',
-    format: 'flat',
-    authorCount: 1,
-  },
-  {
-    author: 'BAVO',
-    role: 'editor',
-    title: 'Cultural Activism Today. The Art of Over-Identification',
-    title_sort: 'Cultural Activism Today. The Art of Over-Identification',
-    file: 'metadata.opf',
-    format: 'calibre',
-    authorCount: 1,
-  },
-  {
-    author: 'BAVO',
-    role: 'editor',
-    title: 'Cultural Activism Today. The Art of Over-Identification',
-    title_sort: 'Cultural Activism Today. The Art of Over-Identification',
-    file: 'BAVO (eds.) - Cultural Activism Today. The Art of Over-Identification.opf',
-    format: 'flat',
-    authorCount: 1,
-  },
-  {
-    author: 'Susan Buck-Morss',
-    role: undefined,
-    title: 'Hegel y Haití. La dialéctica amo-esclavo, una interpretación revolucionaria (Spanish)',
-    title_sort: 'Hegel y Haití. La dialéctica amo-esclavo, una interpretación revolucionaria (Spanish)',
-    file: 'metadata.opf',
-    format: 'calibre',
-    authorCount: 1,
-  },
-  {
-    author: 'Susan Buck-Morss',
-    role: undefined,
-    title: 'Hegel y Haití. La dialéctica amo-esclavo, una interpretación revolucionaria (Spanish)',
-    title_sort: 'Hegel y Haití. La dialéctica amo-esclavo, una interpretación revolucionaria (Spanish)',
-    file: 'Buck-Morss, Susan - Hegel y Haití. La dialéctica amo-esclavo, una interpretación revolucionaria (Spanish).opf',
-    format: 'flat',
-    authorCount: 1,
-  },
-  {
-    author: 'Edward Said',
-    role: undefined,
-    title: 'After Colonialism: Imperial Histories and Postcolonial Displacements',
-    title_sort: 'After Colonialism: Imperial Histories and Postcolonial Displacements',
-    file: 'metadata.opf',
-    format: 'calibre',
-    authorCount: 1,
-  },
-  {
-    author: 'Edward Said',
-    role: undefined,
-    title: 'After Colonialism: Imperial Histories and Postcolonial Displacements',
-    title_sort: 'After Colonialism: Imperial Histories and Postcolonial Displacements',
-    file: 'Said, Edward - After Colonialism: Imperial Histories and Postcolonial Displacements.opf',
-    format: 'flat',
-    authorCount: 1,
-  },
-  {
-    author: 'Boris Buden',
-    role: undefined,
-    title: 'Uvod u prošlost (Serbian)',
-    title_sort: 'Uvod u prošlost (Serbian)',
-    file: 'Buden, Boris; Žilnik, Želimir; kuda.org, et al. - Uvod u prošlost (Serbian).opf',
-    format: 'flat',
-    authorCount: 3,
-  },
-];
 
 describe('Cardcat parsers', () => {
 
-  for (let i = 0; i < correctData.length; i++) {
-    context('parsing ' + validPaths[i], () => {
-      const parsed = parseEntry(validPaths[i]);
+  for (let i = 0; i < testData.length; i++) {
+    context('parsing ' + testData[i].test, () => {
+      const parsed = parseEntry(testData[i].test);
 
-      it('has the format "' + correctData[i].format + '"', () => {
-        expect(parsed.format).to.eql(correctData[i].format);
+      it('has the format "' + testData[i].format + '"', () => {
+        expect(parsed.format).to.eql(testData[i].format);
       });
 
-      it('has the author "' + correctData[i].author + '"', () => {
-        expect(parsed.authors[0].author).to.eql(correctData[i].author);
+      it('has the author "' + testData[i].author + '"', () => {
+        expect(parsed.authors[0].author).to.eql(testData[i].author);
       });
 
-      it('has the right number of authors: "' + correctData[i].authorCount + '"', () => {
-        expect(parsed.authors.length).to.eql(correctData[i].authorCount);
+      it('has the sorted author "' + testData[i].author_sort + '"', () => {
+        expect(parsed.authors[0].author_sort).to.eql(testData[i].author_sort);
       });
 
-      it('has the role "' + correctData[i].role + '"', () => {
-        expect(parsed.authors[0].role).to.eql(correctData[i].role);
+      it('has the right number of authors: "' + testData[i].authorCount + '"', () => {
+        expect(parsed.authors.length).to.eql(testData[i].authorCount);
       });
 
-      it('has the title "' + correctData[i].title + '"', () => {
-        expect(parsed.title).to.eql(correctData[i].title);
+      it('has the role "' + testData[i].role + '"', () => {
+        expect(parsed.authors[0].role).to.eql(testData[i].role);
       });
 
-      it('has the title_sort "' + correctData[i].title_sort + '"', () => {
-        expect(parsed.title_sort).to.eql(correctData[i].title_sort);
+      it('has the title "' + testData[i].title + '"', () => {
+        expect(parsed.title).to.eql(testData[i].title);
       });
 
-      it('has the file "' + correctData[i].file + '"', () => {
-        expect(parsed.file).to.eql(correctData[i].file);
+      it('has the title_sort "' + testData[i].title_sort + '"', () => {
+        expect(parsed.title_sort).to.eql(testData[i].title_sort);
+      });
+
+      it('has the file "' + testData[i].file + '"', () => {
+        expect(parsed.file).to.eql(testData[i].file);
       });
     });
   }
@@ -204,6 +242,7 @@ describe('Cardcat formatters', () => {
     context('formatting paths ' + i, () => {
       const formattedCalibre = formatPath(formattedPaths[i].authors, formattedPaths[i].title, formattedPaths[i].file, 'calibre');
       const formattedFlat = formatPath(formattedPaths[i].authors, formattedPaths[i].title, formattedPaths[i].file, 'flat');
+      const formattedOML = formatPath(formattedPaths[i].authors, formattedPaths[i].title, formattedPaths[i].file, 'oml');
 
       it('"calibre" has the path "' + formattedPaths[i].calibre + '"', () => {
         expect(formattedCalibre).to.eql(formattedPaths[i].calibre);
@@ -211,6 +250,10 @@ describe('Cardcat formatters', () => {
 
       it('"flat" has the path "' + formattedPaths[i].flat + '"', () => {
         expect(formattedFlat).to.eql(formattedPaths[i].flat);
+      });
+
+      it('"oml" has the path "' + formattedPaths[i].oml + '"', () => {
+        expect(formattedOML).to.eql(formattedPaths[i].oml);
       });
 
     });
